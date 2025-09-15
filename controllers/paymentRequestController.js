aee5af63ffa0ddd50f5873f266a5ab9927fb5699
jibindardoc <jibin.cherian@dardoc.com>
2025-09-15 17:30:09 +0530

    [DECLINED, PAID] getPaymentRequests

--- controllers/paymentRequestController.js
+++ controllers/paymentRequestController.js
@@ -52,7 +52,7 @@ exports.getPaymentRequests = async (req, res) => {
     // 1️⃣ Fetch all requests involving this user
     const requests = await PaymentRequest.find({
       $or: [ { receiverUserUUID: userUUID }],
-      status: { $ne: "DECLINED" }   // 👈 exclude declined
+      status: { $ne:["DECLINED", "PAID"]  }   // 👈 exclude declined
     }).sort({ createdAt: -1 });
 
     // 2️⃣ Enrich with user details and readable date

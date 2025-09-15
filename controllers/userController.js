a015fff5dde2a2ac85552268daa94e9fd73a4d17
jibindardoc <jibin.cherian@dardoc.com>
2025-09-15 10:34:43 +0530

    senderUserUUID

--- controllers/userController.js
+++ controllers/userController.js
@@ -175,7 +175,6 @@ exports.createUser = async (req, res) => {
 };
 
 // Update user image by userUUID
-// Update user image by userUUID
 exports.updateUserImage = async (req, res) => {
   try {
     const { userUUID, imageUrl } = req.body;
